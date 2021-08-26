00e2d7b92d3acd59adffb5724cb18cdaf5412fe4
Jie Chen <79719408+jiechen919293@users.noreply.github.com>
2021-08-26 01:06:41 -0500

    leetcode88(2)-merge2arr
    Success
    Details 
    Runtime: 68 ms, faster than 90.87% of JavaScript online submissions for Merge Sorted Array.
    Memory Usage: 39 MB, less than 42.45% of JavaScript online submissions for Merge Sorted Array.
    Next challenges:

---
+++ leetcode88(2)-merge.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+const merge = function(nums1, m, nums2, n) {
+    nums1.splice(m);
+    nums2.splice(n);
+    nums1.push(...nums2);
+    nums1.sort((a, b) => a - b);
+
+};
+merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3);
+merge([1], 1, [], 0);
+merge([], 0, [1], 1);
