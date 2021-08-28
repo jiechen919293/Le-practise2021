8d0b5dcd18db349329905fa51b30164d4e246b0a
Jie Chen <79719408+jiechen919293@users.noreply.github.com>
2021-08-27 23:56:51 -0500

    leetcode198-house robber
    not pass need to debug

---
+++ leetcode198-house robber.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+const rob = function(nums) {
+    if (nums.length == 0) return 0;
+    let temperArr = new Array(nums.length);
+    temperArr[1] = nums[0];
+    for (let i = 2; i <= nums.length; ++i) {
+        temperArr[i] = temperArr[i - 1] > nums[i - 1] + temperArr[i - 2] ? temperArr[i - 1] : nums[i - 1] + temperArr[i - 2];
+        console.log(temperArr[i]);
+    };
+    return temperArr[nums.length];
+};
+let numstest = [2, 7, 9, 3, 1];
+let numstest1 = [1, 2, 3, 1];
+console.log(rob(numstest));
+console.log("_________________")
+console.log(rob(numstest1));
