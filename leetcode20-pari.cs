659c1b34c3db52bdffaef850e5dc2c66c4ee32a8
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-07 23:36:04 -0500

    leetcode20-pari
    Success
    Details 
    Runtime: 68 ms, faster than 95.02% of C# online submissions for Valid Parentheses.
    Memory Usage: 22.5 MB, less than 59.89% of C# online submissions for Valid Parentheses.

---
+++ leetcode20-pari.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    public class Solution
+    {
+        public bool IsValid(string s)
+        {
+            if (s.Length == 0 || s.Length % 2 == 1)
+            {
+                return false;
+            }
+            Stack<char> testStack = new Stack<char>();
+            for (int i = 0; i <s.Length; i++)
+            {
+                if( s[i]=='('|| s[i] == '['|| s[i] == '{')
+                {
+                    testStack.Push(s[i]);
+                }
+                else
+                {
+                    if (testStack.Count == 0) return false;
+                    if (s[i] == ')' && testStack.Pop() != '(') return false;
+                    if (s[i] == '}' && testStack.Pop() != '{') return false;
+                    if (s[i] == ']' && testStack.Pop() != '[') return false;
+
+                }
+            }
+            return testStack.Count == 0 ? true : false;
+        }
+    }
+}
