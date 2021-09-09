530a28769b608a1a829fc3d59002a89b99b38592
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-08 23:04:51 -0500

    eetcode277findcelebrity(2)
    CSDN new solution

---
+++ Leetcode277findcelebrity(2).cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode277findcelebrity_2_
+    {
+        public static bool knows(int x, int i)
+        {
+            return true;
+        }
+        public int findCelebrity(int n)
+        {
+            for (int i = 0; i < n; ++i)
+            {
+                bool isCelebrity = true;
+                for (int j = 0; j < n; ++j)
+                {
+                    if (j == i) continue;
+                    if (knows(i, j) || !knows(j, i))
+                    {
+                        isCelebrity = false;
+                        break;
+                    }
+                }
+                if (isCelebrity)
+                    return i;
+            }
+            return -1;
+        }
+ }
+}
