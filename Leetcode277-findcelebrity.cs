fc7c63e76bcebf1b1c9fd38101dad80d9d88f099
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-08 22:45:59 -0500

    Leetcode277-findcelebrity(zuo shen suanf )

---
+++ Leetcode277-findcelebrity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode277_findcelebrity
+    {
+		// 提交时不要提交这个函数，只提交下面的方法//zuo 神算法
+		public static bool knows(int x, int i)
+		{
+			return true;
+		}
+
+		public int findCelebrity(int n)
+		{
+			int cand = 0;
+			for (int i = 0; i < n; ++i)
+			{
+				if (knows(cand, i))
+				{
+					cand = i;
+				}
+			}
+			for (int i = 0; i < cand; ++i)
+			{
+				if (knows(cand, i))
+				{
+					return -1;
+				}
+			}
+			for (int i = 0; i < n; ++i)
+			{
+				if (!knows(i, cand))
+				{
+					return -1;
+				}
+			}
+			return cand;
+		}
+	}
+}
