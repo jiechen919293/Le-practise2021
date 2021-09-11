b5853094ac3c7559c15a99618c0c30a3dbba9864
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-10 23:32:24 -0500

    Leetcode26-removeElem
    Success
    Details 
    Runtime: 248 ms, faster than 66.95% of C# online submissions for Remove Duplicates from Sorted Array.
    Memory Usage: 33.9 MB, less than 69.70% of C# online submissions for Remove Duplicates from Sorted Array.
    Next challenges:

---
+++ Leetcode26-removeElem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode26_removeElem
+    {
+        public int RemoveElement(int[] nums)
+        {
+			if (nums == null)
+			{
+				return 0;
+			}
+			if (nums.Length < 2)
+			{
+				return nums.Length;
+			}
+			int done = 0;
+			for (int i = 1; i < nums.Length; i++)
+			{
+				if (nums[i] != nums[done])
+				{
+					nums[++done] = nums[i];
+				}
+			}
+			return done + 1;
+		}
+	}
+    
+}
