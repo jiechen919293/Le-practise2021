94690766948d1093667e048e5eeaae9b4a41c072
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-11 23:50:29 -0500

    Leetcode2-add2nums 左神答案
    Success
    Details 
    Runtime: 231 ms, faster than 5.08% of C# online submissions for Add Two Numbers.
    Memory Usage: 28.2 MB, less than 81.28% of C# online submissions for Add Two Numbers.

---
+++ Leetcode2-add2nums.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode2_add2nums
+    {
+        /**
+   * Definition for singly-linked list.   */
+   public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+            {
+   
+            int ca = 0;
+            int n1 = 0;
+            int n2 = 0;
+            int n = 0;
+            ListNode c1 = l1;
+            ListNode c2 = l2;
+            ListNode node = null;
+            ListNode pre = null;
+            while (c1 != null || c2 != null)
+            {
+                n1 = c1 != null ? c1.val : 0;
+                n2 = c2 != null ? c2.val : 0;
+                n = n1 + n2 + ca;
+                pre = node;
+                node = new ListNode(n % 10);
+                node.next = pre;
+                ca = n / 10;
+                c1 = c1 != null ? c1.next : null;
+                c2 = c2 != null ? c2.next : null;
+            }
+            if (ca == 1)
+            {
+                pre = node;
+                node = new ListNode(1);
+                node.next = pre;
+            }
+            return reverseList(node);
+        }
+      public ListNode reverseList(ListNode head){
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null)
+        {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+    }
+   
+    
+}
