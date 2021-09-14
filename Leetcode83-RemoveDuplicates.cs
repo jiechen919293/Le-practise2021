ca8da886612df5337aa7245f29665eaeaa641f4b
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-13 23:41:26 -0500

    Leetcode83-RemoveDuplicates
    Success
    Details 
    Runtime: 156 ms, faster than 10.14% of C# online submissions for Remove Duplicates from Sorted List.
    Memory Usage: 26.3 MB, less than 88.19% of C# online submissions for Remove Duplicates from Sorted List.

---
+++ Leetcode83-RemoveDuplicates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    /**
+ * Definition for singly-linked list.
+ * */
+     public class ListNode {
+     public int val;
+     public ListNode next;
+     public ListNode(int val=0, ListNode next=null) {
+         this.val = val;
+          this.next = next;
+      }
+  }
+ 
+    class Leetcode83_RemoveDuplicates
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            ListNode resultNode = head;
+         
+            while (resultNode != null&&resultNode.next!=null)
+            {
+                if (resultNode.val == resultNode.next.val)
+                {
+                    resultNode.next = resultNode.next.next;
+                }
+                else
+                {
+                    resultNode = resultNode.next;
+                }
+
+            }
+            return head;
+        }
+    }
+}
