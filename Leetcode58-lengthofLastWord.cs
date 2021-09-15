bd60c4c516168ce60656e320dcd6345b6813e12f
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-14 23:05:28 -0500

    Leetcode58-lengthofLastWord
    Success
    Details 
    Runtime: 126 ms, faster than 10.89% of C# online submissions for Length of Last Word.
    Memory Usage: 22.5 MB, less than 95.73% of C# online submissions for Length of Last Word.

---
+++ Leetcode58-lengthofLastWord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode58_lengthofLastWord
+    {
+        //            var list = s.Trim().Split(' ');
+         //   return list.Last().Length;
+        public int LengthOfLastWord(string s)
+        {
+            s = s.Trim();
+            int result = 0;
+            for (int i = s.Length-1; i >= 0; i--)
+            {
+                if(s[i]==' ')
+                {
+                 return result;
+                }
+                else
+                {
+                    result++;
+                }
+               
+            }
+            return result;
+        }
+    
+    }
+ 
+}
