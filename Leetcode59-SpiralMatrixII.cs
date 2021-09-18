b96e554517299212b79e9226a27fda3f326b5f1c
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-17 23:46:21 -0500

    Leetcode59-SpiralMatrixII
    Not pass yet need update

---
+++ Leetcode59-SpiralMatrixII.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode59_SpiralMatrixII
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            int[][] result = new int[n][];
+            int top = 0;
+            int left = 0;
+            int right = n ;
+            int bottom = n;
+            int num = 1;
+            while (left<=right && top<=bottom)
+            {
+                for (int i = left; i < right; i++)
+                {
+                    result[top][i] = ++num;
+                }
+                top++;
+                for (int i = top; i < bottom; i++)
+                {
+                    result[i][right] = ++num;
+                }
+                right--;
+                for (int i = right; i >= left; i--)
+                {
+                    result[bottom][i] = ++num;
+                }
+                bottom--;
+
+                for (int i = bottom; i >= top; i--)
+                {
+                    result[i][left] = ++num;
+                }
+                left++;
+            }
+
+            return result;
+        }
+
+    }
+}
