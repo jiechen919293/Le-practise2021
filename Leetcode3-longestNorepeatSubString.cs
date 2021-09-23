18ed68028db72737ec1b5115b6e8a5dce85dffbe
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-22 23:35:24 -0500

    Leetcode3-longestNorepeatSubString
    need adjust runtime issue

---
+++ Leetcode3-longestNorepeatSubString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode3_longestNorepeatSubString
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int result = 0;
+            if (s.Length == 0) return result;
+            int[] temp = new int[256];
+            int left = 0;
+            int right = -1;
+            while (left<s.Length)
+            {
+                if (right + 1 < s.Length && temp[s[right + 1] - 'a'] == 0)
+                {
+                    temp[s[right + 1] - 'a']++;
+                    right++;
+                }
+                else
+                {
+                    temp[s[left] - 'a']--;
+                        left++;
+                }
+                result = Math.Max(result, right - left - 1);
+            }
+            return result;
+
+        }
+    }
+}
