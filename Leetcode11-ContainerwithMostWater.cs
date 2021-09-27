bd9aa1592a928365191d8a04de58bf09db9c7d95
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-26 23:45:45 -0500

    Leetcode11-ContainerwithMostWater
    Runtime: 204 ms, faster than 51.88% of C# online submissions for Container With Most Water.
    Memory Usage: 44.2 MB, less than 82.28% of C# online submissions for Container With Most Water.

---
+++ Leetcode11-ContainerwithMostWater.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode11_ContainerwithMostWater
+    {
+        //Given n non-negative integers a1, a2, ..., an ,
+        //where each represents a point at coordinate(i, ai). n vertical lines are drawn such
+        //that the two endpoints of the line i is at(i, ai) and(i, 0). Find two lines, which,
+        // together with the x-axis forms a container, such that the container contains the most water.
+        public int MaxArea(int[] height)
+        {
+            int result = 0;
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right)
+            {
+                result = Math.Max(result, Math.Min(height[left], height[right]) * (right - left));
+                if (height[left] > height[right])
+                {
+                    right--;
+                }
+                else
+                {
+                    left++;
+                }
+            }
+            return result;
+        }
+    }
+}
