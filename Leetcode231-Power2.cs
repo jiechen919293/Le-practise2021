1ae71dda3f41e4e83e8259fb2d24710398c2a337
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-09-29 23:59:23 -0500

    Leetcode231-Power2 --need update

---
+++ Leetcode231-Power2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+   // Given an integer n, return true if it is a power of two.Otherwise, return false.
+
+
+   //An integer n is a power of two, if there exists an integer x such that n == 2x.
+    class Leetcode231_Power2
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            bool result = true;
+
+            return result;
+
+        }
+    }
+}
