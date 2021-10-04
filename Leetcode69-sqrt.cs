e29b81a7d2dd77552bdb0617d572751d31c6e822
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-10-03 23:47:56 -0500

    Leetcode69-sqrt version2 use func inside
    Success
    Details 
    Runtime: 40 ms, faster than 86.41% of C# online submissions for Sqrt(x).
    Memory Usage: 15.3 MB, less than 62.31% of C# online submissions for Sqrt(x).

---
+++ Leetcode69-sqrt.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode69_sqrt
+    {
+    }
+}
