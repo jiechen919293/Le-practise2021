43a238c84e639dce9f0d554c5d84be9f224977bd
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-10-25 23:15:30 -0500

    Leetcode226-InvertBinTree
    Success
    Details 
    Runtime: 153 ms, faster than 18.25% of C# online submissions for Invert Binary Tree.
    Memory Usage: 36.1 MB, less than 20.53% of C# online submissions for Invert Binary Tree.

---
+++ Leetcode226-InvertBinTree.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode226_InvertBinTree
+    {
+
+        //Definition for binary tree node
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+            {
+                this.val = val;
+                this.left = left;
+                this.right = right;
+            }
+
+            //function for tree 
+            public TreeNode InvertTree(TreeNode root)
+            {
+                if (root == null) return null;
+                TreeNode tempNode = root.left;
+                root.left = root.right;
+                root.right = tempNode;
+                InvertTree(root.left);
+                InvertTree(root.right);
+                
+                return root;
+
+            }
+        }
+    }
+}
