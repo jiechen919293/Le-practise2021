9270def5d43ab630a57194f3174cb2cdb227db55
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-10-28 23:34:55 -0500

    Leetcode994-RottingOranges
    Not finish , need to do later

---
+++ Leetcode994-RottingOranges.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+//    You are given an m x n grid where each cell can have one of three values:
+
+//0 representing an empty cell,
+//1 representing a fresh orange, or
+//2 representing a rotten orange.
+//Every minute, any fresh orange that is 4-directionally adjacent to a rotten orange becomes rotten.
+
+//Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1.
+//Input: grid = [[2,1,1],[1,1,0],[0,1,1]]
+//Output: 4
+// example 2
+//    Input: grid = [[2,1,1],[0,1,1],[1,0,1]]
+//Output: -1
+//Explanation: The orange in the bottom left corner(row 2, column 0) is never rotten, because rotting only happens 4-directionally.
+
+//        Example 3:
+
+//Input: grid = [[0,2]]
+//Output: 0
+//Explanation: Since there are already no fresh oranges at minute 0, the answer is just 0.
+    class Leetcode994_RottingOranges
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            //remember this ,should finish tomorrow
+        }
+    }
+}
