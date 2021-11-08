6a36de7728e92ee48748c0b52274528017c16a51
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-11-07 22:38:44 -0600

    Leetcode96-UniqueBInarySearchTree
    这是一个排列组合的问题， 用暴力 double for 循环
    Details 
    Runtime: 36 ms, faster than 80.46% of C# online submissions for Unique Binary Search Trees.
    Memory Usage: 26.8 MB, less than 26.05% of C# online submissions for Unique Binary Search Trees.

---
+++ Leetcode96-UniqueBInarySearchTree.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    //Given an integer n, return the number of structurally unique BST's
+    // (binary search trees) which has exactly n nodes of unique values from 1 to n.
+    class Leetcode96_UniqueBInarySearchTree
+    {
+        public int NumTrees(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 1;
+
+            int[] nums = new int[n + 1];
+            nums[0] = 1; nums[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    nums[i] = nums[i] + nums[j] * nums[i - 1 - j];
+                }
+            }
+            return nums[n];
+ 
+        }
+    }
+}
