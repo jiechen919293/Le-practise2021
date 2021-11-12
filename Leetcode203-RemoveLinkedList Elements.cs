0a5a29f0da63abd04ba4dbd4e14ee911c8bbd90d
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-11-11 22:50:57 -0600

    Leetcode203-RemoveLinkedList Elements
    Success
    Details 
    Runtime: 104 ms, faster than 47.92% of C# online submissions for Remove Linked List Elements.
    Memory Usage: 41.1 MB, less than 14.43% of C# online submissions for Remove Linked List Elements.
    Next challenges:

---
+++ Leetcode203-RemoveLinkedList Elements.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+
+
+
+    class Leetcode203_RemoveLinkedList_Elements
+    {
+        //Definition for singly-linked list.
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int val = 0, ListNode next = null)
+            {
+                this.val = val;
+                this.next = next;
+            }
+        }
+
+        //beging the process
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode temp = new ListNode();
+            temp.next = head;
+            var next = temp;
+            while (next.next != null)
+            {
+                if (next.next.val = val)
+                {
+                    next.next = next.next.next;
+                }
+                else
+                {
+                    next = next.next;
+                }
+            }
+            return temp.next;
+        }
+    }
+}
