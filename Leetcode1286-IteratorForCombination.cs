622c0c84861d5831bd44af5e5d8af636417d7d2f
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-11-13 22:46:14 -0600

    Leetcode1286-IteratorForCombinatio-NOT FINNISH
    Not finish,

---
+++ Leetcode1286-IteratorForCombination.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace leetcode2021
+{
+    class Leetcode1286_IteratorForCombination
+    {
+        public CombinationIterator(string characters, int combinationLength)
+        {
+            var s = characters;
+            var bits = (1 << s.Length) - 1;
+            
+        }
+        public int Count(int n)
+        {
+            int count = 0;
+            while (n)
+            {
+                count++;
+                n = n & (n - 1);
+            }
+            return count;
+        }
+        public string Next()
+        {
+
+        }
+
+        public bool HasNext()
+        {
+
+        }
+    }
+}
