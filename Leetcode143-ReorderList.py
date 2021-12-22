014d46f579e09f62359146c44f06b967bac9aa12
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-12-22 00:12:36 -0600

    this is the result for Leetcode143
    Success
    Details 
    Runtime: 92 ms, faster than 71.08% of Python3 online submissions for Reorder List.
    Memory Usage: 23.4 MB, less than 16.26% of Python3 online submissions for Reorder List.
    Next challenges:

--- Leetcode143-ReorderList.py
+++ Leetcode143-ReorderList.py
@@ -3,6 +3,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
+
 class Solution:
     def reorderList(self, head: Optional[ListNode]) -> None:
         """
