ebb29c2180e7e9115625ad3799667f5f7f26bc12
Jie Chen（Joy） <79719408+jiechen919293@users.noreply.github.com>
2021-12-22 00:10:52 -0600

    Update Leetcode143-ReorderList.py
    turn into list and show it as new tree

--- Leetcode143-ReorderList.py
+++ Leetcode143-ReorderList.py
@@ -8,3 +8,25 @@ class Solution:
         """
         Do not return anything, modify head in-place instead.
         """
+        if not head:
+            return
+
+        # 线性表存储链表节点
+        lst = []
+        node = head
+        while node:
+            lst.append(node)
+            node = node.next
+        
+        # 定义指针指向首尾，开始取节点重建链表
+        start = 0
+        end = len(lst) - 1
+        while start < end:
+            # 重新排列
+            lst[start].next = lst[end]
+            start += 1
+            if start == end:
+                break
+            
+            lst[end].next = lst[start]
+            end -= 1
